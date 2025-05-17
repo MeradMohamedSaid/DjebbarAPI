aa5944e91a7a0f14baeac3fa4c17e14658e5f1b7
Mohamed <meradmohamedsaid@gmail.com>
2025-05-17 22:57:19 +0100

    finalS

---
+++ api/datetime.js
@@ -0,0 +1,25 @@
+// api/datetime.js
+
+const { DateTime } = require("luxon");
+
+export default function handler(req, res) {
+  const dt = DateTime.now().setZone("Africa/Algiers");
+
+  const response = {
+    year: dt.year,
+    month: dt.month,
+    day: dt.day,
+    hour: dt.hour,
+    minute: dt.minute,
+    seconds: dt.second,
+    milliSeconds: dt.millisecond,
+    dateTime: dt.toISO({ includeOffset: false }),
+    date: dt.toFormat("MM/dd/yyyy"),
+    time: dt.toFormat("HH:mm"),
+    timeZone: dt.zoneName,
+    dayOfWeek: dt.weekdayLong,
+    dstActive: dt.isInDST,
+  };
+
+  res.status(200).json(response);
+}
